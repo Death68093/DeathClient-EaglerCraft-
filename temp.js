b78dab3cf2b21ba30b205369c7c957749ad90d2b
Death68093 <hlcahill15@gmail.com>
2025-11-13 10:34:16 -0600

    Add player information module to temp.js

---
+++ temp.js
@@ -0,0 +1,8 @@
+const mod = ModAPI
+mod.meta.title("GetPlayer")
+mod.meta.description("Just tells you everything about the player")
+mod.meta.credits("By Death68093\n - Creator of Everything...")
+mod.meta.version("V1.0")
+
+mod.require("player")
+mod.displayToChat(mod.player)
